b15107246171f22ada905c5f01881a8dd2cbb63e
Ying Liu <yingliu4203@gmail.com>
2014-09-13 11:03:19 -0700

    add tests to init

--- connector8/tests/__init__.py
+++ connector8/tests/__init__.py
@@ -1,10 +1,16 @@
 # -*- coding: utf-8 -*-
 
 import test_session
+import test_event
+import test_producer
+
 
 fast_suite = [
 ]
 
+
 checks = [
     test_session,
+    test_event,
+    test_producer,
 ]
