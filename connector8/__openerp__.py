f7029ee7702f8f966e58ab34a3da17f572baf1c5
Ying Liu <yingliu4203@gmail.com>
2014-09-19 12:23:32 -0700

    remove  checkpoint_view.xml and connector menu

--- connector8/__openerp__.py
+++ connector8/__openerp__.py
@@ -14,8 +14,6 @@ This is a port of OCA connector to Odoo 8.0
           'security/ir.model.access.csv',
           'queue/model_view.xml',
           'queue/queue_data.xml',
-          #'checkpoint/checkpoint_view.xml',
-          'connector_menu.xml',
           'setting_view.xml',
           'res_partner_view.xml',
           ],
