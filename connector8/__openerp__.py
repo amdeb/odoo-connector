73cac3f940662507f96554f24c11f7b5dae9b400
Ying Liu <yingliu4203@gmail.com>
2014-09-19 17:14:45 -0700

    add 'checkpoint/checkpoint_view.xml',

--- connector8/__openerp__.py
+++ connector8/__openerp__.py
@@ -14,6 +14,7 @@ This is a port of OCA connector to Odoo 8.0
           'security/ir.model.access.csv',
           'queue/model_view.xml',
           'queue/queue_data.xml',
+          'checkpoint/checkpoint_view.xml',
           'res_partner_view.xml',
           ],
  'installable': True,
