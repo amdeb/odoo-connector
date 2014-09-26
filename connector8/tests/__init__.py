2f9c0f5226c57210938a668754755a3da2ba0acf
Ying Liu <yingliu4203@gmail.com>
2014-09-25 18:08:14 -0700

    add tests

--- connector8/tests/__init__.py
+++ connector8/tests/__init__.py
@@ -2,9 +2,13 @@
 
 import test_session
 import test_event
+import test_job
+import test_queue
+import test_backend
 import test_producer
 import test_connector
-import test_backend
+import test_mapper
+import test_related_action
 
 
 fast_suite = [
@@ -13,7 +17,11 @@ fast_suite = [
 checks = [
     test_session,
     test_event,
+    test_job,
+    test_queue,
+    test_backend,
     test_producer,
     test_connector,
-    test_backend,
+    test_mapper,
+    test_related_action,
 ]
